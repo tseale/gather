2fdac43c4348aafad83dcc67df74b261ecf691b3
cjhin <csjhin@gmail.com>
2013-07-27 00:51:39 +0000

    changed button callbacks and ids in the API

--- server/docs/public/parse_json.js
+++ server/docs/public/parse_json.js
@@ -2,35 +2,38 @@ $(document).ready( function() {
 
 	$.getJSON( "api.json", function(data) {
 		$.each(data, function(method_i, method) { //for each method 
+	
+			var url = method["method_url"].slice(1); //slice to take off "/"
 			//populate the method_table
 			var html = 
-			'<a href="#"  onclick="loadInfo('+"'"+method["method_title"]+"'"+');return false;"><div id="'+method["method_title"]+'">'+
-			method["method_title"]+
-			'</div></a>';
+			'<div class="table_button" id="'+url+'">'+
+			"/"+url +
+			'</div>';
 
-			$("#method_table").append(html);
+			$("#method_table").append(html); //add the actual html element
+			$("#"+url).click( function() { //tie in the click to the loadInfo function
+				loadInfo(url);
+			}); 
 		});
 	});
 });
 
 //called when a specific method is selected, populate the main method_description div with info
-function loadInfo(method_title) {
+function loadInfo(url) {
+
+	console.log(url);
 	
+	//set all background colors back to normal
+	$("#method_table").children().css("backgroundColor", "#eee");
+
 	//darken the current selector in the table
-	var tar_str = "#"+method_title;
+	var tar_str = "#"+url;
 	var tar_div = $(tar_str);
 	tar_div.css( "backgroundColor", "#ccc");
-	/*
-	console.log(tar_div);
-	tar_div.animate({
-		backgroundColor: "5px"
-
-	}, 1000, function() {});
-	*/
 
 	$.getJSON( "api.json", function(data) {
 		$.each(data, function(method_i, method) { //for each method, find the correct one 
-			if(method["method_title"]==method_title) {
+			if(method["method_url"].slice(1)==url) {
 				$("#method_description").empty();
 
 				var html = 
